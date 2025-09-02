8abf49eef96d8e74ad4ee9470ff5759cfe6764cf
Veronika Vanitseva <veronika1999.v@gmail.com>
2025-09-02 16:12:11 +0300

    Foulders

---
+++ Shop/Controllers/SpaceshipsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Shop/Models/Spaceships/SpaceshipsIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Shop.Models.Spaceships
+{
+    public class SpaceshipsIndexViewModel
+    {
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+        public string? TypeName { get; set; }
+        public DateTime? BuiltDate { get; set; }
+        public int? Crew { get; set; }
+        public int? EnginePower { get; set; }
+        public int? Passengers { get; set; }
+        public int? InnerVolume { get; set; }
+
+    }
+}
